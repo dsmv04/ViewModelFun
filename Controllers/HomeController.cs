97cc6c5d84be9e5d99831bd64c7eaa21088a9447
dsmv04 <dalton.munoz.v@hotmail.com>
2023-10-26 11:04:03 -0300

    Asignacion 1 dia 3 semana 7

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ViewModelFun.Models;
+
+namespace ViewModelFun.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+     public List<User> GenerateUsers()
+        {
+            return new List<User>()
+            {
+                new User(){ FirstName="Moose", LastName="Phillips"},
+                new User(){ FirstName="Sarah", LastName=""},
+                new User(){ FirstName="Jerry", LastName=""},
+                new User(){ FirstName="Rene", LastName="Ricky"},
+                new User(){ FirstName="Barbarah", LastName=""},
+            };
+        }
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            string stringModel = "My message is here, this is a simple string that I am using as a model";
+
+            return View("Index", stringModel);
+        }
+        [HttpGet("numbers")]
+        public IActionResult Numbers()
+        {
+            int[] numbers = new int[]{1,2,3,10,43,5};
+            return View(numbers);
+        }
+        [HttpGet("users")]
+        public IActionResult AllUsers()
+        {
+            var users = GenerateUsers();
+            return View(users);
+        }
+        [HttpGet("user")]
+        public IActionResult OneUser()
+        {
+            var rand = new Random();
+            var users = GenerateUsers();
+
+            // grab random user (could just create one, grab first, etc...)
+            var user = users[rand.Next(users.Count)];
+            return View(user);
+        }
+}
